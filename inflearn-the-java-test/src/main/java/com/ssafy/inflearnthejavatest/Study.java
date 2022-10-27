3514f05f5f47115b30486bcf0c9ef74c278be292
Yeongseo Kim <dudtjakdl@naver.com>
2022-10-27 17:49:03 +0900

    [S07P31C209-88][BE-영서] test: JUnit 예제 코드 작성 완료

--- inflearn-the-java-test/src/main/java/com/ssafy/inflearnthejavatest/Study.java
+++ inflearn-the-java-test/src/main/java/com/ssafy/inflearnthejavatest/Study.java
@@ -3,13 +3,18 @@ package com.ssafy.inflearnthejavatest;
 public class Study {
 
     private StudyStatus status;
-
     private int limit;
+    private String name;
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
 
     public Study(int limit) {
-//        if (limit < 0) {
-//            throw new IllegalArgumentException("limit은 0보다 커야 한다.");
-//        }
+        if (limit < 0) {
+            throw new IllegalArgumentException("limit은 0보다 커야 한다.");
+        }
         this.limit = limit;
     }
 
@@ -19,5 +24,18 @@ public class Study {
 
     public int getLimit() {
         return limit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
