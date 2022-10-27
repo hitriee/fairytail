f523b18cd42e58c2c74521fc52416b0c59571c29
wonkyoung <pubhan35@gmail.com>
2022-10-27 22:10:30 +0900

    [S07P31C209-94] [FE-원경] chore: 개발 환경 설정

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import logo from './logo.svg';
+import './App.css';
 
-// Screens
-import Main from './screens/Main';
-import MessageCreate from './screens/MessageCreate';
-import MessageDetail from './screens/MessageDetail';
-import MessageList from './screens/MessageList';
-import MessageVR from './screens/MessageVR';
-import Globe from './screens/Globe';
-import Map from './screens/Map';
-import Settings from './screens/Settings';
-
-const Stack = createStackNavigator();
-
-function StackNavigation() {
+function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="MessageCreate" component={MessageCreate} />
-        <Stack.Screen name="MessageDetail" component={MessageDetail} />
-        <Stack.Screen name="MessageList" component={MessageList} />
-        <Stack.Screen name="MessageVR" component={MessageVR} />
-        <Stack.Screen name="Globe" component={Globe} />
-        <Stack.Screen name="Map" component={Map} />
-        <Stack.Screen name="Settings" component={Settings} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-export default StackNavigation;
+export default App;
--- Frontend/src/apis/dummy.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function dummy() {
-  return (
-    '폴더 구조 위해서 만든 더미 파일'
-  )
-}
