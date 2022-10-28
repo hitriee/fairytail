0418ac3316246c08533058f7b8af865558e1fb26
조예지 <celpegor216@gmail.com>
2022-10-28 17:46:50 +0900

    Revert "[S07P31C209-74][FE-예지] feat: MessageCreate 캐러셀 중간 저장"
    This reverts commit 91c3381eea84fd3a9fab5b7e948de887c2f04573

--- frontend/src/screens/MessageCreate.tsx
+++ frontend/src/screens/MessageCreate.tsx
@@ -1,15 +1,14 @@
-import Carousel from "@/components/messageCreate/Carousel";
 import React from "react";
 import "../styles/_utils.scss";
 import "./MessageCreate.scss";
 
 function MessageCreate() {
   return (
-    <div className="Screen">
-      <div className="Container">
-        <Carousel />
+    <>
+      <div id="create">
+        <h1>하이용!</h1>
       </div>
-    </div>
+    </>
   );
 }
 
