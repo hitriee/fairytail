91c3381eea84fd3a9fab5b7e948de887c2f04573
조예지 <97578535+celpegor216@users.noreply.github.com>
2022-10-28 17:44:57 +0900

    [S07P31C209-74][FE-예지] feat: MessageCreate 캐러셀 중간 저장

--- frontend/src/screens/MessageCreate.tsx
+++ frontend/src/screens/MessageCreate.tsx
@@ -1,14 +1,15 @@
+import Carousel from "@/components/messageCreate/Carousel";
 import React from "react";
 import "../styles/_utils.scss";
 import "./MessageCreate.scss";
 
 function MessageCreate() {
   return (
-    <>
-      <div id="create">
-        <h1>하이용!</h1>
+    <div className="Screen">
+      <div className="Container">
+        <Carousel />
       </div>
-    </>
+    </div>
   );
 }
 
