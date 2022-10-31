08f4cf03576333d3227a6327dcfec77c63e4e054
조예지 <97578535+celpegor216@users.noreply.github.com>
2022-10-31 17:57:23 +0900

    [S07P31C209-133][FE-예지] feat: VR 중간 저장

--- frontend/src/screens/VR.tsx
+++ frontend/src/screens/VR.tsx
@@ -1,29 +1,139 @@
 import 'aframe';
-import {Scene, Box, Sphere, Cylinder, Plane, Sky} from '@belivvr/aframe-react';
+import {
+  Scene,
+  Box,
+  Camera,
+  Assets,
+  Image,
+  Cursor,
+  Sky,
+  Entity,
+} from '@belivvr/aframe-react';
+import {emojiArr} from '../assets/emojis';
+
+AFRAME.registerComponent('clickhandler', {
+  schema: {
+    txt: {default: 'default'},
+  },
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    el.addEventListener('click', function () {
+      console.log(data.txt);
+    });
+  },
+});
 
 function VR() {
   return (
-    <Scene>
+    <Scene cursor="rayOrigin: mouse">
+      <Assets>
+        <img
+          id="city"
+          src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg"
+        />
+        <img id="test" src={emojiArr[0]} alt="테스트" />
+      </Assets>
+
+      <Sky id="" src="#city" />
+
+      <Entity />
+
+      <Image
+        src="#test"
+        opacity={1}
+        width={1}
+        position={{x: 1, y: 0.7, z: 0}}
+        billboard
+      />
+
       <Box
-        position={{x: -1, y: 0.5, z: -3}}
+        position={{x: -5, y: 10, z: -5}}
         rotation={{x: 0, y: 45, z: 0}}
-        color="#4CC3D9"
+        color="#ff3c00"
+        clickhandler="txt:box1"
       />
-      <Sphere position={{x: 0, y: 1.25, z: -5}} radius={1.25} color="#EF2D5E" />
-      <Cylinder
-        position={{x: 1, y: 0.75, z: -3}}
-        radius={0.5}
-        height={1.5}
-        color="#FFC65D"
+      <Box
+        position={{x: 5, y: 10, z: -5}}
+        rotation={{x: 0, y: 45, z: 0}}
+        color="#ffa200"
+        clickhandler="txt:box2"
       />
-      <Plane
-        position={{x: 0, y: 0, z: -4}}
-        rotation={{x: -90, y: 0, z: 0}}
-        width={4}
-        height={4}
-        color="#7BC8A4"
+      <Box
+        position={{x: -5, y: 10, z: 5}}
+        rotation={{x: 0, y: 45, z: 0}}
+        color="#f2ff00"
+        clickhandler="txt:box3"
       />
-      <Sky color="#ECECEC" />
+      <Box
+        position={{x: 5, y: 10, z: 5}}
+        rotation={{x: 0, y: 45, z: 0}}
+        color="#3cff00"
+        clickhandler="txt:box4"
+      />
+      <Box
+        position={{x: -5, y: 0, z: -5}}
+        rotation={{x: 0, y: 45, z: 0}}
+        color="#00fbff"
+        clickhandler="txt:box5"
+      />
+      <Box
+        position={{x: 5, y: 0, z: -5}}
+        rotation={{x: 0, y: 45, z: 0}}
+        color="#0026ff"
+        clickhandler="txt:box6"
+      />
+      <Box
+        position={{x: -5, y: 0, z: 5}}
+        rotation={{x: 0, y: 45, z: 0}}
+        color="#960afa"
+        clickhandler="txt:box7"
+      />
+      <Box
+        position={{x: 5, y: 0, z: 5}}
+        rotation={{x: 0, y: 45, z: 0}}
+        color="#ff00bf"
+        clickhandler="txt:box8"
+      />
+
+      <Camera
+        near={1}
+        wasdControlsEnabled={false}
+        orbit-controls="
+        autoRotate: true;
+        autoRotateSpeed: -0.1;
+        rotateSpeed: 0.1;
+        target: 0 0 0;
+        initialPosition: 0 0 0;
+        minPolarAngle: 0;
+        maxPolarAngle: 90;
+      ">
+        <Cursor
+          id="cursor"
+          animation__click={{
+            property: 'scale',
+            from: '0.1 0.1 0.1',
+            to: '1 1 1',
+            easing: 'easeInCubic',
+            dur: 150,
+            startEvents: ['click'],
+          }}
+          animation__clickreset={{
+            property: 'scale',
+            to: '0.1 0.1 0.1',
+            dur: 1,
+            startEvents: ['animationcomplete__click'],
+          }}
+          animation__fusing={{
+            property: 'scale',
+            from: '1 1 1',
+            to: '0.1 0.1 0.1',
+            easing: 'easeInCubic',
+            dur: 150,
+            startEvents: ['fusing'],
+          }}
+        />
+      </Camera>
     </Scene>
   );
 }
