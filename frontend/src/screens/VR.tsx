084eb97789a6ea231392809ab60035ed1ad267d9
조예지 <97578535+celpegor216@users.noreply.github.com>
2022-10-31 13:36:13 +0900

    [S07P31C209-133][FE-예지] fix: 불필요한 파일 삭제

--- frontend/src/screens/VR.tsx
+++ frontend/src/screens/VR.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import 'aframe';
+import {Scene, Box, Sphere, Cylinder, Plane, Sky} from '@belivvr/aframe-react';
 
 function VR() {
   return (
-    <>
-      <p>This Screen is VR</p>
-    </>
+    <Scene>
+      <Box
+        position={{x: -1, y: 0.5, z: -3}}
+        rotation={{x: 0, y: 45, z: 0}}
+        color="#4CC3D9"
+      />
+      <Sphere position={{x: 0, y: 1.25, z: -5}} radius={1.25} color="#EF2D5E" />
+      <Cylinder
+        position={{x: 1, y: 0.75, z: -3}}
+        radius={0.5}
+        height={1.5}
+        color="#FFC65D"
+      />
+      <Plane
+        position={{x: 0, y: 0, z: -4}}
+        rotation={{x: -90, y: 0, z: 0}}
+        width={4}
+        height={4}
+        color="#7BC8A4"
+      />
+      <Sky color="#ECECEC" />
+    </Scene>
   );
 }
 
