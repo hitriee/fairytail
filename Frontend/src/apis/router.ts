9969864b22d9a829e81eccfa5e8adccedf28b04d
조예지 <celpegor216@gmail.com>
2022-10-31 10:37:48 +0900

    Deleted Frontend/public/favicon.ico, Frontend/public/index.html, Frontend/public/logo192.png, Frontend/public/logo512.png, Frontend/public/manifest.json, Frontend/public/robots.txt, Frontend/src/apis/router.ts, Frontend/src/apis/server.ts, Frontend/src/assets/images/earthlights.jpg, Frontend/src/assets/images/earthmap.jpg, Frontend/src/assets/images/map.png, Frontend/src/assets/images/resolution.jpg, Frontend/src/assets/images/star.jpg, Frontend/src/assets/images/stars.jpg, Frontend/src/assets/images/stars2.jpg, Frontend/src/components/globe/Sphere.tsx, Frontend/src/components/globe/Stars.tsx, Frontend/src/navigations/MainStack.tsx, Frontend/src/screens/Globe.tsx, Frontend/src/screens/Intro.tsx, Frontend/src/screens/Loading.tsx, Frontend/src/screens/Main.tsx, Frontend/src/screens/Map.tsx, Frontend/src/screens/MessageCreate.tsx, Frontend/src/screens/MessageDetail.tsx, Frontend/src/screens/MessageList.tsx, Frontend/src/screens/MessageVR.tsx, Frontend/src/screens/Settings.tsx, Frontend/src/.prettierrc.js, Frontend/src/App.css, Frontend/src/App.test.tsx, Frontend/src/App.tsx, Frontend/src/index.css, Frontend/src/index.tsx, Frontend/src/logo.svg, Frontend/src/react-app-env.d.ts, Frontend/src/reportWebVitals.ts, Frontend/src/setupTests.ts, Frontend/.gitignore, Frontend/README.md, Frontend/package-lock.json, Frontend/package.json, Frontend/tsconfig.json

--- Frontend/src/apis/router.ts
+++
@@ -1,8 +0,0 @@
-export const main = () => '/main';
-export const intro = () => '/';
-export const globe = () => '/globe';
-export const map = () => '/map';
-export const vr = () => '/vr';
-export const messageList = () => '/message/list';
-export const messageCreate = () => '/message/create';
-export const messageDetail = () => '/message/detail';
