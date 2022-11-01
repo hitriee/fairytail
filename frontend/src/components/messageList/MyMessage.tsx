40f242b0f317555acc81ae2b5cfce14c507509da
leesungjo <lsj2837@naver.com>
2022-11-01 12:01:43 +0900

    [S07P31C209-3][FE-이성조] 메세지 리스트 퍼블리싱 완료

--- frontend/src/components/messageList/MyMessage.tsx
+++ frontend/src/components/messageList/MyMessage.tsx
@@ -20,13 +20,12 @@ function MyMessage({item}: itemProps) {
         <img src={emojiArr[item.emoji]} alt="emoji" />
       </div>
       <div className="myMessage-container">
-        {/* <p className="myMessage-container-id">{item.id}</p> */}
         <p className="myMessage-container-title">{item.title}</p>
-        <span className="myMessage-container-contents">{item.contents}</span>
-        <span className="myMessage-container-like">
-          <img src={emojiArr[62]} alt="emoji" />
-          {item.like}
-        </span>
+        <span className="myMessage-container-date">{item.contents}</span>
+      </div>
+      <div className="myMessage-container-like">
+        <img src={emojiArr[62]} alt="emoji" />
+        {item.like}
       </div>
     </div>
   );
