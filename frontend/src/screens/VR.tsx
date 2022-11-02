6c3f5ed16d9cca2e225f23c9b69ce4e8fd9d778e
조예지 <97578535+celpegor216@users.noreply.github.com>
2022-11-02 11:14:23 +0900

    [S07P31C209-133][FE-예지] feat: 이모지 폴더 변경

--- frontend/src/screens/VR.tsx
+++ frontend/src/screens/VR.tsx
@@ -4,7 +4,12 @@ import Iframe from 'react-iframe';
 function VR() {
   return (
     <div className="vr">
-      <Iframe className="vr-frame" url="vr.html" src="vr.html" />
+      <Iframe
+        className="vr-frame"
+        url="VR.html"
+        src="VR.html"
+        frameBorder={0}
+      />
     </div>
   );
 }
