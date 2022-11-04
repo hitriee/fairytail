132773e556b71041ad4d2905a59c628857a6eb97
junghojin <hojinnjung@naver.com>
2022-11-04 11:43:50 +0900

    [S07P31C209-187][BE-호진] feat: Text service skeleton & Swagger API 설정

--- msa/text/src/main/java/com/fairytail/text/security/WebSecurity.java
+++ msa/text/src/main/java/com/fairytail/text/security/WebSecurity.java
@@ -10,7 +10,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter  {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/alert/**").permitAll();
+        http.authorizeRequests().antMatchers("/text/**").permitAll();
 
         /** h2 데이터베이스 조회 시, 프레임 엑스박스 현상 - 추후 삭제 예정 */
         http.headers().frameOptions().disable();
