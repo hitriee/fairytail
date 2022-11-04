c90dac61eeb7c9d49635c917792d5ce1f8f0611e
junghojin <hojinnjung@naver.com>
2022-11-04 11:06:27 +0900

    [S07P31C209-190][BE-호진] feat: Voice service skeleton & Swagger API 설정

--- msa/voice/src/main/java/com/fairytail/voice/controller/VoiceController.java
+++ msa/voice/src/main/java/com/fairytail/voice/controller/VoiceController.java
@@ -1,11 +1,28 @@
 package com.fairytail.voice.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "voice", tags = {"swagger", "v1", "api"})
 @RestController
 @RequestMapping("/voice")
 @RequiredArgsConstructor
 public class VoiceController {
+
+    private final Environment env;
+
+    @ApiOperation(value = "Service 상태 체크", notes = "Text Service 상태 체크를 위한 API 입니다.")
+    /** Service 상태 체크 (삭제 금지!) */
+    @GetMapping("/health_check")
+    public String status() {
+        // Random으로 할당된 포트 번호 받아오기
+        return String.format("Text service is working on port %s!",
+                env.getProperty("local.server.port"));
+    }
+
 }
