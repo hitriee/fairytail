fc3dac88693e1f67018d36fb4e4a99459ee6e82d
wonkyoung <pubhan35@gmail.com>
2022-11-04 10:03:42 +0900

    [S07P31C209-157] [FE-원경] style: 알림 전체 삭제 확인 및 디자인 보완

--- frontend/src/components/individual/MyNotification.tsx
+++ frontend/src/components/individual/MyNotification.tsx
@@ -1,4 +1,4 @@
-import {emojiArrStatic} from 'src/assets/emojis';
+import {emojiArr} from 'src/assets/emojis';
 import '@individual/MyNotifications.scss';
 import {useNavigate} from 'react-router';
 
@@ -12,21 +12,25 @@ interface itemProps {
 
 function MyNotification({item}: itemProps) {
   const navigate = useNavigate();
+  const {id, title, emoji} = item;
   const toDetail = (postId: number) => {
     return () => navigate(`/message/detail/${postId}`);
   };
+  const shortTitle = () => {
+    if (title.length >= 5) {
+      return `${title.slice(0, 7)}...`;
+    } else {
+      return title;
+    }
+  };
   return (
-    <div className="myNotification" onClick={toDetail(item.id)}>
-      <div className="myNotification-emoji">
-        <img src={emojiArrStatic[item.emoji]} alt="emojiArrStatic" />
-      </div>
-      <div className="myNotification-container">
-        <p className="myNotification-container-title">
-          익명의 작가가 당신의 이야기 <br />
-          <span>{item.title.slice(0, 5)}...</span>을 좋아합니다
-        </p>
-        {/* <span className="myMessage-container-date">{item.contents}</span> */}
-      </div>
+    <div className="myNotification" onClick={toDetail(id)}>
+      <img src={emojiArr[emoji]} alt="emoji" className="myNotification-emoji" />
+      <p>
+        익명의 작가가 당신의 이야기 <br />
+        <span className="myNotification-title">{shortTitle()}</span>을
+        좋아합니다
+      </p>
     </div>
   );
 }
