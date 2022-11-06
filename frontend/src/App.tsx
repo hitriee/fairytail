d695d90163c51813a84b365fc116ecd0e42efb69
이성조 <lsj2837@naver.com>
2022-11-06 23:41:49 +0900

    [S07P31C209-222][FE-이성조] style : 통합 로딩 css 조정중

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {Suspense} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import '@/App.scss';
-import Loader from '../src/components/room/Loading';
+import Loading from '../src/components/loading/Loading';
 // route
 import Intro from '@screens/Intro';
 import Main from '@screens/Main';
@@ -29,25 +29,30 @@ import {
   notifications,
 } from '@apis/router';
 
+//recoil
+import {RecoilRoot} from 'recoil';
+
 function App() {
   return (
     <BrowserRouter>
-      {/* <Suspense fallback={<Loader />}> */}
-      <Routes>
-        <Route path={intro()} element={<Intro />} />
-        <Route path={main()} element={<Main />} />
-        <Route path={globe()} element={<Globe />} />
-        <Route path={map()} element={<Map />} />
-        <Route path={vr()} element={<VR />} />
-        <Route path={messageList()} element={<MessageList />} />
-        <Route path={messageCreate()} element={<MessageCreate />} />
-        <Route path={messageUpdate()} element={<MessageCreate />} />
-        <Route path={messageDetail()} element={<MessageDetail />} />
-        <Route path={notFound()} element={<NotFound />} />
-        <Route path={settings()} element={<Individual />} />
-        <Route path={notifications()} element={<Individual />} />
-      </Routes>
-      {/* </Suspense> */}
+      <RecoilRoot>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path={intro()} element={<Intro />} />
+            <Route path={main()} element={<Main />} />
+            <Route path={globe()} element={<Globe />} />
+            <Route path={map()} element={<Map />} />
+            <Route path={vr()} element={<VR />} />
+            <Route path={messageList()} element={<MessageList />} />
+            <Route path={messageCreate()} element={<MessageCreate />} />
+            <Route path={messageUpdate()} element={<MessageCreate />} />
+            <Route path={messageDetail()} element={<MessageDetail />} />
+            <Route path={notFound()} element={<NotFound />} />
+            <Route path={settings()} element={<Individual />} />
+            <Route path={notifications()} element={<Individual />} />
+          </Routes>
+        </Suspense>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
--- frontend/src/components/loading/Loading.tsx
+++ frontend/src/components/loading/Loading.tsx
@@ -1,12 +1,23 @@
-import "./Loading.scss";
-import loading from "../../assets/images/loading.png";
-
+import React from 'react';
+import './Loading.scss';
+import Iframe from 'react-iframe';
+import loading from '../../assets/images/loading.png';
+// import Background from 'public/background/Background.html'
+// import '../../../public/background/Background.html'
 function Loading() {
   return (
-    <div className="loading-background">
-      <img className="loading-image" src={loading} alt="로딩 이미지" />
-      <p className="loading-message">~이동 중~</p>
-    </div>
+    <>
+      <Iframe
+        className="iframeLoading"
+        url="../../background/Background.html"
+        src="../../background/Background.html"
+        frameBorder={0}
+      />
+      <div className="loading-background">
+        <img className="loading-image" src={loading} alt="로딩 이미지" />
+        <p className="loading-message">~이동 중~</p>
+      </div>
+    </>
   );
 }
 
