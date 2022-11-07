c5bb8d05bf59e311d2db09b4015e8a880d9c214f
stopone2639 <stopone2639@naver.com>
2022-11-07 16:25:20 +0900

    [S07P31C209-234][BE-정지원] feat : 이미지 CRUD 기능 개발

--- msa/img/src/main/java/com/fairytail/img/service/ImgService.java
+++ msa/img/src/main/java/com/fairytail/img/service/ImgService.java
@@ -1,4 +1,12 @@
 package com.fairytail.img.service;
 
+import com.fairytail.img.dto.ImgDto;
+
+import java.io.IOException;
+
 public interface ImgService {
+    ImgDto createImg(ImgDto imgDto) throws Exception;
+    ImgDto readImg(Long postId) throws Exception;
+    ImgDto putImg(ImgDto imgDto) throws Exception;
+    Boolean deleteImg(Long postId) throws Exception;
 }
