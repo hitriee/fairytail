d2e23960708750b95bf3c28bf30cb617ed991b59
stopone2639 <stopone2639@naver.com>
2022-11-07 11:54:45 +0900

    [S07P31C209-233][BE-정지원] feat : s3 Config, Util 작성

---
+++ msa/img/src/main/java/com/fairytail/img/util/S3Util.java
@@ -0,0 +1,52 @@
+package com.fairytail.img.util;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Component
+@RequiredArgsConstructor
+public class S3Util {
+    @Value("${cloud.front.name}")
+    private String cloudFrontName;
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String getCloudFrontName(){
+        return cloudFrontName;
+    }
+
+    public String upload(File file, String dirName){
+        String fileName = dirName+"/"+file.getName();
+        boolean exist = amazonS3Client.doesObjectExist(bucket, fileName);
+        if(exist == true){
+            amazonS3Client.deleteObject(bucket, fileName);
+        }
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, file).withCannedAcl(CannedAccessControlList.Private));
+        return cloudFrontName + "/" + fileName;
+    }
+
+    public void delete(File file, String dirName){
+        String fileName = dirName+"/" + file.getName();
+        boolean exist = amazonS3Client.doesObjectExist(bucket, fileName);
+        if(exist == true){
+            amazonS3Client.deleteObject(bucket, fileName);
+        }
+    }
+
+    public void update(String oldFilePath, File file, String dirName){
+        boolean exist = amazonS3Client.doesObjectExist(bucket, oldFilePath);
+        if(exist == true){
+            amazonS3Client.deleteObject(bucket, oldFilePath);
+        }
+        upload(file, dirName);
+    }
+}
