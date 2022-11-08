493aad49756491a52015eb81030da414e7b26fb3
junghojin <hojinnjung@naver.com>
2022-11-08 11:30:39 +0900

    [S07P31C209-243][BE-호진] fix: Swagger 로딩 오류 해결

--- msa/user/src/main/java/com/fairytail/user/controller/UserController.java
+++ msa/user/src/main/java/com/fairytail/user/controller/UserController.java
@@ -3,8 +3,6 @@ package com.fairytail.user.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@Api(value = "user", tags = {"api"})
+@Api(value = "user")
 @RestController
 public class UserController {
 
