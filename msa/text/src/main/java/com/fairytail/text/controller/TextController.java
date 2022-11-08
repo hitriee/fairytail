2ed5048724bd7cf596fd55481f7788d5c61ccf13
junghojin <hojinnjung@naver.com>
2022-11-08 11:45:27 +0900

    [S07P31C209-145][BE-호진] fix: Text service Swagger 설정

--- msa/text/src/main/java/com/fairytail/text/controller/TextController.java
+++ msa/text/src/main/java/com/fairytail/text/controller/TextController.java
@@ -1,7 +1,6 @@
 package com.fairytail.text.controller;
 
 import com.fairytail.text.dto.TextDto;
-import com.fairytail.text.jpa.TextEntity;
 import com.fairytail.text.mapper.TextMapper;
 import com.fairytail.text.service.TextService;
 import com.fairytail.text.vo.TextRequest;
@@ -11,13 +10,19 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Api(value = "text", tags = {"swagger", "v1", "api"})
+import javax.annotation.Resource;
+
+@Api(value = "text")
 @RestController
 @RequiredArgsConstructor
 public class TextController {
-    private final Environment env;
+    @Resource
+    private Environment env;
 
     private final TextMapper textMapper;
 
