c0589e2122035096fe71ddc2cc96880269919373
junghojin <hojinnjung@naver.com>
2022-11-08 11:54:38 +0900

    [S07P31C209-233][BE-호진] fix: Swagger 로딩 오류 해결

--- msa/img/src/main/java/com/fairytail/img/config/SwaggerConfig.java
+++ msa/img/src/main/java/com/fairytail/img/config/SwaggerConfig.java
@@ -17,10 +17,10 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false) // Swagger 에서 제공해주는 기본 응답 코드 (200, 401, 403, 404) 등의 노출 여부
+                .useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드 (200, 401, 403, 404) 등의 노출 여부
                 .apiInfo(apiInfo()) // Swagger UI 로 노출할 정보
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.fairytail.user")) // api 스펙이 작성되어 있는 패키지 (controller)
+                .apis(RequestHandlerSelectors.basePackage("com.fairytail.img")) // api 스펙이 작성되어 있는 패키지 (controller)
                 .paths(PathSelectors.any()) // apis 에 위치하는 API 중 특정 path 를 선택
                 .build();
     }
