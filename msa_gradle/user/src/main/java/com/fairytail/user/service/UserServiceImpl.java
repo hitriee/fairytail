bee6d2f7088aab606e4df6dc5f6b869cc6c8edf3
junghojin <hojinnjung@naver.com>
2022-11-09 10:21:48 +0900

    [No issue][BE-호진] fix: user-port 번호 변경

--- msa_gradle/user/src/main/java/com/fairytail/user/service/UserServiceImpl.java
+++ msa_gradle/user/src/main/java/com/fairytail/user/service/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.fairytail.user.service;
 
+import com.fairytail.user.jpa.UserEntity;
+import com.fairytail.user.jpa.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
 }
