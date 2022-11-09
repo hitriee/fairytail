21522b37466566aac27e779f7ebc7f62c86f2f2e
stopone2639 <stopone2639@naver.com>
2022-11-09 16:18:15 +0900

    [S07P31C209-233][BE-정지원] chore : url에서 s3 경로 뽑는 유틸 예외처리

--- msa_gradle/img/src/main/java/com/fairytail/img/util/MainUtil.java
+++ msa_gradle/img/src/main/java/com/fairytail/img/util/MainUtil.java
@@ -18,6 +18,9 @@ public class MainUtil {
 
     public String urlToFilePath(String url){
         String[] sp = url.split("/");
-        return sp[sp.length -2] + "/" + sp[sp.length -1];
+        if(sp.length >= 2){
+            return sp[sp.length -2] + "/" + sp[sp.length -1];
+        }
+        return null;
     }
 }
