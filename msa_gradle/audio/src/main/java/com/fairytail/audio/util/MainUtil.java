0936bf629512bd7656f195da55aff5b261dfe91d
stopone2639 <stopone2639@naver.com>
2022-11-11 10:00:38 +0900

    [S07P31C209-240][BE-정지원] chore : url에서 경로 빼기 예외 추가

--- msa_gradle/audio/src/main/java/com/fairytail/audio/util/MainUtil.java
+++ msa_gradle/audio/src/main/java/com/fairytail/audio/util/MainUtil.java
@@ -17,7 +17,7 @@ public class MainUtil {
     }
 
     public String urlToFilePath(String url){
-        if(url != null){
+        if(url != null && url != ""){
             String[] sp = url.split("/");
             if(sp.length >= 2){
                 return sp[sp.length -2] + "/" + sp[sp.length -1];
