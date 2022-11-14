449f964a5b6e163dd8ceb5e08957d2f74a95544d
junghojin <hojinnjung@naver.com>
2022-11-14 14:42:35 +0900

    [S07P31C209-251][BE-호진] feat: Firebase Token 저장 API

--- msa_gradle/user/src/main/java/com/fairytail/user/controller/UserController.java
+++ msa_gradle/user/src/main/java/com/fairytail/user/controller/UserController.java
@@ -1,25 +1,28 @@
 package com.fairytail.user.controller;
 
-import com.fairytail.user.config.MapperConfig;
 import com.fairytail.user.dto.UserDto;
 import com.fairytail.user.service.UserService;
 import com.fairytail.user.vo.RequestFirebase;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.net.http.HttpRequest;
 
 @Api(value = "user")
 @RestController
+@Log4j2
 @RequiredArgsConstructor
 public class UserController {
 
@@ -44,7 +47,8 @@ public class UserController {
         UserDto userDto = modelMapper.map(requestFirebase, UserDto.class);
 
         Integer result = userService.saveFirebaseToken(userDto);
-        if(result != 1) {
+        log.debug(result);
+        if(result < 1) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save firebase token.");
         }
         return ResponseEntity.status(HttpStatus.OK).body("Succeeded to save firebase token.");
