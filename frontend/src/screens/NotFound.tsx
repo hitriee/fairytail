908da898d0390ceeb55fbcfe496ee0156920c4a2
wonkyoung <pubhan35@gmail.com>
2022-11-14 22:05:25 +0900

    [S07P31C209-275] [FE-원경] style: not Found 페이지 디자인 및 문구 수정

--- frontend/src/screens/NotFound.tsx
+++ frontend/src/screens/NotFound.tsx
@@ -1,16 +1,22 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import IntroLogo from '@images/introLogo.png';
 import '@screens/NotFound.scss';
+import {main} from '@/apis/router';
 
 function NotFound() {
+  const navigate = useNavigate();
+  const toMain = () => navigate(main());
   return (
     <div className="notFound">
-      <div className="white"> 404 Not Found</div>
+      <div className="white notFound-title notFound-content">
+        길을 잃었습니다
+      </div>
       <img src={IntroLogo} />
-      <div className="white">페이지를 찾을 수 없습니다</div>
-      <Link to="/main">
-        <button>go Main</button>
-      </Link>
+      {/* <Link to="/main"> */}
+      <button className="notFound-content notFound-button" onClick={toMain}>
+        내 방으로 돌아가기
+      </button>
+      {/* </Link> */}
     </div>
   );
 }
