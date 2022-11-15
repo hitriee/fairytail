3797dde389f04008e05a38bc659e72431fa1bd48
조예지 <97578535+celpegor216@users.noreply.github.com>
2022-11-15 17:58:25 +0900

    [No Issue][FE-예지] chore: 최초 접근 시 도움말 표시 중

--- frontend/src/components/common/OpenHelp.tsx
+++ frontend/src/components/common/OpenHelp.tsx
@@ -1,10 +1,11 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 import '@common/Common.scss';
 import {ReactComponent as Help} from '@images/help.svg';
 import helpMap from '@images/helpMap.jpg';
 import helpMain from '@images/helpMain.jpg';
 import helpVR from '@images/helpVR.jpg';
+import {useLocation} from 'react-router-dom';
 
 interface OpenHelpProps {
   imagesIndex: number;
@@ -19,6 +20,31 @@ function OpenHelp({imagesIndex, color = 'white'}: OpenHelpProps) {
 
   const closeHelp = () => setIsOpened(false);
 
+  // 현재 경로 가져오기
+  const pathname = useLocation().pathname;
+
+  useEffect(() => {
+    // 방문한 경로 목록 가져오기
+    const visitedHelpJson = localStorage.getItem('visitedHelp');
+    let visitedHelpArr = [];
+    if (visitedHelpJson !== null) {
+      visitedHelpArr = JSON.parse(visitedHelpJson);
+    }
+
+    // 현재 경로가 방문한 경로 목록에 없다면 추가, 도움말 표시
+    if (!visitedHelpArr?.includes(pathname)) {
+      visitedHelpArr?.push(pathname);
+      localStorage.setItem('visitedHelp', JSON.stringify(visitedHelpArr));
+      if (pathname === '/main') {
+        setTimeout(() => {
+          setIsOpened(true);
+        }, 3200);
+      } else {
+        setIsOpened(true);
+      }
+    }
+  }, [pathname]);
+
   return (
     <>
       <div
@@ -29,7 +55,7 @@ function OpenHelp({imagesIndex, color = 'white'}: OpenHelpProps) {
         <Help viewBox="0 0 45 50" width="40" height="40" fill={color} />
       </div>
       {isOpened ? (
-        <div className="help-background" onClick={closeHelp}>
+        <div className="help-background fadeIn" onClick={closeHelp}>
           <div className="container help-container">
             <img
               className="help-img"
