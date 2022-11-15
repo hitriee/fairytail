b0dd4000711ebd6fca099240775a17318469660b
Yeongseo Kim <dudtjakdl@naver.com>
2022-11-15 10:59:21 +0900

    [S07P31C209-207][BE-김영서] chore: 타임존 설정

--- msa_gradle/text/src/main/java/com/fairytail/text/TextApplication.java
+++ msa_gradle/text/src/main/java/com/fairytail/text/TextApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class TextApplication {
+
+	@PostConstruct
+	public void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TextApplication.class, args);
