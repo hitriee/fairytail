eaf9e6c14cdb8b4b84ec3c1e19b95782d820de62
Yeongseo Kim <dudtjakdl@naver.com>
2022-11-15 23:24:33 +0900

    [No issue][BE-김영서] chore: cors config 설정 추가

--- msa_gradle/gateway/src/main/java/com/fairytail/gateway/security/WebConfig.java
+++ msa_gradle/gateway/src/main/java/com/fairytail/gateway/security/WebConfig.java
@@ -47,3 +47,19 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     }
 }
 */
+
+package com.fairytail.gateway.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://k7c209.p.ssafy.io", "https://k7c2091.p.ssafy.io");
+    }
+}
+
