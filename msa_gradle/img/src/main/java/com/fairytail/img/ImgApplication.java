1191f3f5c1e02f2170f4024414efe69518f70858
junghojin <hojinnjung@naver.com>
2022-11-15 10:01:03 +0900

    [S07P31C209-247][BE-호진] feat: IMG microservice 등록

--- msa_gradle/img/src/main/java/com/fairytail/img/ImgApplication.java
+++ msa_gradle/img/src/main/java/com/fairytail/img/ImgApplication.java
@@ -2,9 +2,10 @@ package com.fairytail.img;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ImgApplication {
 
 	public static void main(String[] args) {
