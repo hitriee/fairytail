ed46ac9be832e2f1197f9cf582cc7e3c6b7f216c
junghojin <hojinnjung@naver.com>
2022-11-15 14:32:47 +0900

    [S07P31C209-247][BE-호진] feat: Text microservice 등록

--- msa_gradle/text/src/main/java/com/fairytail/text/TextApplication.java
+++ msa_gradle/text/src/main/java/com/fairytail/text/TextApplication.java
@@ -2,13 +2,14 @@ package com.fairytail.text;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class TextApplication {
 
 	@PostConstruct
