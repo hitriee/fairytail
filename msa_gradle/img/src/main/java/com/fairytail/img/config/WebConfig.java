816b602215f1b606f740582211049ec8961ce22f
junghojin <hojinnjung@naver.com>
2022-11-17 20:33:12 +0900

    [S07P31C209-249][BE-호진] fix: CORS 해결

--- msa_gradle/img/src/main/java/com/fairytail/img/config/WebConfig.java
+++ msa_gradle/img/src/main/java/com/fairytail/img/config/WebConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.fairytail.img.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -12,4 +13,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000", "https://k7c209.p.ssafy.io", "https://k7c2091.p.ssafy.io")
                 .allowedMethods("*");
     }
-}
+}*/
