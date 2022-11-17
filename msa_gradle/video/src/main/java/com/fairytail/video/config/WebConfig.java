2927e8ed3c8f7af7a145e06589e8f4db28c6ba9a
junghojin <hojinnjung@naver.com>
2022-11-17 20:31:56 +0900

    [S07P31C209-249][BE-호진] fix: Cors 해결

--- msa_gradle/video/src/main/java/com/fairytail/video/config/WebConfig.java
+++ msa_gradle/video/src/main/java/com/fairytail/video/config/WebConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.fairytail.video.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,4 +12,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000", "https://k7c209.p.ssafy.io", "https://k7c2091.p.ssafy.io")
                 .allowedMethods("*");
     }
-}
+}*/
