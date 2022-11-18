1347dbf1d4769355c40b391a6bffe47751a1f295
조예지 <celpegor216@gmail.com>
2022-11-18 16:26:54 +0900

    [No Issue][FE-예지] fix: 음성 자막 표시 수정

--- frontend/src/components/messageCreate/MusicPlayer.tsx
+++ frontend/src/components/messageCreate/MusicPlayer.tsx
@@ -2,7 +2,7 @@ import React, {useState, useRef} from 'react';
 import styled from 'styled-components';
 
 import '@messageCreate/MusicPlayer.scss';
-import subtitle from '@images/speechToText.svg';
+import subtitleImg from '@images/speechToText.svg';
 import {ReactComponent as Play} from '@images/play.svg';
 import {ReactComponent as Pause} from '@images/pause.svg';
 
@@ -86,7 +86,7 @@ function MusicPlayer({fileURL, subtitle, isDetail}: MusicPlayerProps) {
     <div className="music-player-container">
       {isDetail ? (
         <img
-          src={subtitle}
+          src={subtitleImg}
           alt="자막 표시"
           className="music-player-subtitle-btn"
           onClick={() => setIsShowingSubtitle(prev => !prev)}
