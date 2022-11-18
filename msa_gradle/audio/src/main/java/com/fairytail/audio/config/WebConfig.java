e316a36e45befb2c7b9d43994a8a162ec61a9a85
junghojin <hojinnjung@naver.com>
2022-11-18 09:37:01 +0900

    [S07P31C209-249][BE-호진] fix: CORS 해결

--- msa_gradle/audio/src/main/java/com/fairytail/audio/config/WebConfig.java
+++ msa_gradle/audio/src/main/java/com/fairytail/audio/config/WebConfig.java
@@ -1,4 +1,4 @@
-/*
+
 package com.fairytail.audio.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +13,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000", "https://k7c209.p.ssafy.io", "https://k7c2091.p.ssafy.io")
                 .allowedMethods("*");
     }
-}*/
+}
 
