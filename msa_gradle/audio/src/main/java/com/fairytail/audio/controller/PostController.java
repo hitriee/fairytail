86d5b89580a6b2946d0b1b2b0f9489f59a476794
Yeongseo Kim <dudtjakdl@naver.com>
2022-11-19 14:47:16 +0900

    [No issue][BE-김영서] fix: map과 vr에 자신이 쓴 비공개 메시지 포함되도록 수정

--- msa_gradle/audio/src/main/java/com/fairytail/audio/controller/PostController.java
+++ msa_gradle/audio/src/main/java/com/fairytail/audio/controller/PostController.java
@@ -150,11 +150,11 @@ public class PostController {
      */
     @ApiOperation(value = "근처 게시글 최신순 리스트 조회", notes = "근처 게시글 최신순 리스트 조회 API 입니다.")
     @GetMapping("/vr/latest")
-    public ResponseEntity<?> readPostListLatest(@RequestParam Double lat, @RequestParam Double lng) throws Exception{
+    public ResponseEntity<?> readPostListLatest(@RequestParam Double lat, @RequestParam Double lng, @RequestParam Long userId) throws Exception{
         ModelMapper modelMapper = new ModelMapper();
         resultMap = new HashMap<>();
         status = HttpStatus.INTERNAL_SERVER_ERROR;
-        List<PostDto> res = postService.readPostListLatest(lat, lng);
+        List<PostDto> res = postService.readPostListLatest(lat, lng, userId);
         List<ResponsePostList> data = new ArrayList<>();
         for (PostDto r:res) {
             ResponsePostList insert = modelMapper.map(r, ResponsePostList.class);
@@ -177,11 +177,11 @@ public class PostController {
      */
     @ApiOperation(value = "근처 게시글 좋아요 순 리스트 조회", notes = "근처 게시글 좋아요 순 리스트 조회 API 입니다.")
     @GetMapping("/vr/like")
-    public ResponseEntity<?> readPostListLike(@RequestParam Double lat, @RequestParam Double lng) throws Exception{
+    public ResponseEntity<?> readPostListLike(@RequestParam Double lat, @RequestParam Double lng, @RequestParam Long userId) throws Exception{
         ModelMapper modelMapper = new ModelMapper();
         resultMap = new HashMap<>();
         status = HttpStatus.INTERNAL_SERVER_ERROR;
-        List<PostDto> res = postService.readPostListLike(lat, lng);
+        List<PostDto> res = postService.readPostListLike(lat, lng, userId);
         List<ResponsePostList> data = new ArrayList<>();
         for (PostDto r : res){
             ResponsePostList insert = modelMapper.map(r, ResponsePostList.class);
@@ -231,11 +231,11 @@ public class PostController {
      * 전체 게시글 좌표 조회
      */
     @GetMapping("/map")
-    public ResponseEntity<?> readAll() throws Exception{
+    public ResponseEntity<?> readAll(@RequestParam Long userId) throws Exception{
         ModelMapper modelMapper = new ModelMapper();
         resultMap = new HashMap<>();
         status = HttpStatus.INTERNAL_SERVER_ERROR;
-        List<PostDto> res = postService.readAllPost();
+        List<PostDto> res = postService.readAllPost(userId);
         List<ResponsePostMap> data = new ArrayList<>();
         for(PostDto r : res){
             ResponsePostMap d = modelMapper.map(r, ResponsePostMap.class);
