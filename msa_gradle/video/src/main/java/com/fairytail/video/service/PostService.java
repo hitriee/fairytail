35a4656ed94e47d6197a1d5482172332d9ec154a
Yeongseo Kim <dudtjakdl@naver.com>
2022-11-19 14:51:56 +0900

    [No issue][BE-김영서] fix: map과 vr에 자신이 쓴 비공개 메시지 포함되도록 수정

--- msa_gradle/video/src/main/java/com/fairytail/video/service/PostService.java
+++ msa_gradle/video/src/main/java/com/fairytail/video/service/PostService.java
@@ -16,8 +16,8 @@ public interface PostService {
     PostDto putPost(PostDto postDto) throws Exception; //게시글 공개, 비공개 수정 
     Boolean deletePost(Long postId) throws Exception; //게시글 삭제
 
-    List<PostDto> readPostListLatest(Double lat, Double lng) throws Exception; //주변 최신순 리스트 조회
-    List<PostDto> readPostListLike(Double lat, Double lng) throws Exception; //주변 좋아요순 리스트 조회
+    List<PostDto> readPostListLatest(Double lat, Double lng, Long userId) throws Exception; //주변 최신순 리스트 조회
+    List<PostDto> readPostListLike(Double lat, Double lng, Long userId) throws Exception; //주변 좋아요순 리스트 조회
     List<PostDto> readMyPostList(Long userId) throws Exception; //내가 작성한 리스트 조회 
 
     Boolean createLike(PostLikeDto postLikeDto) throws Exception; //좋아요 누르기, 취소
@@ -26,7 +26,7 @@ public interface PostService {
 
     Boolean changeStatus(PostEntity post)throws Exception; //게시글이 5회 이상 신고 받으면 게시글을 잠금 상태로 변경
 
-    List<PostDto> readAllPost() throws Exception; //전체 지역의 모든 리스트 조회
+    List<PostDto> readAllPost(Long userId) throws Exception; //전체 지역의 모든 리스트 조회
 
     Integer detectSafeSearch(InputStream inputStream) throws IOException;
 }
