bae83fc6a7dc8538f509e313bd486aafe8957f9b
조예지 <celpegor216@gmail.com>
2022-11-20 16:42:31 +0900

    [No Issue][FE-예지] chore: 캐러셀 lazy loading 시도

--- frontend/src/components/messageCreate/Carousel.tsx
+++ frontend/src/components/messageCreate/Carousel.tsx
@@ -2,7 +2,7 @@ import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import SwiperCore, {EffectCoverflow} from 'swiper';
 import {Swiper, SwiperSlide} from 'swiper/react';
-import {useState, Dispatch, SetStateAction, useEffect} from 'react';
+import {useState, Dispatch, SetStateAction, useEffect, useRef} from 'react';
 
 import '@messageCreate/Carousel.scss';
 import {emojiArr} from '@emojis/index';
@@ -13,6 +13,48 @@ type CarouselProps = {
   onSlideChange: Dispatch<SetStateAction<number>>;
   setIsLongClicked: Dispatch<SetStateAction<boolean>>;
 };
+
+type CaroueslImageProps = {
+  item: string;
+  index: number;
+};
+
+function CarouselImage({item, index}: CaroueslImageProps) {
+  const imageRef = useRef<HTMLImageElement>(null);
+  const observerRef = useRef<IntersectionObserver>();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  function onIntersection(
+    entries: IntersectionObserverEntry[],
+    io: IntersectionObserver,
+  ) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        io.unobserve(entry.target);
+        setIsLoaded(true);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (!observerRef.current) {
+      observerRef.current = new IntersectionObserver(onIntersection, {
+        threshold: 0.01,
+      });
+    }
+
+    imageRef.current && observerRef.current.observe(imageRef.current);
+  });
+
+  return (
+    <img
+      ref={imageRef}
+      className="emoji"
+      src={isLoaded ? item : ''}
+      alt={`${index}번 이모지`}
+    />
+  );
+}
 
 // 캐러셀
 function Carousel({onSlideChange, setIsLongClicked, emojiNo}: CarouselProps) {
@@ -46,12 +88,7 @@ function Carousel({onSlideChange, setIsLongClicked, emojiNo}: CarouselProps) {
       {emojiArr.map((item, index) => {
         return (
           <SwiperSlide key={index} {...longPress}>
-            <img
-              className="emoji"
-              src={item}
-              alt={`${index}번 이모지`}
-              loading="lazy"
-            />
+            <CarouselImage item={item} index={index} />
           </SwiperSlide>
         );
       })}
